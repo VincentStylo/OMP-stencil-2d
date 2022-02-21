be351dee0650d7dd0fcb3198c4dca65b185f3af3
Vincent Stylo <j.martinez.torres@icloud.com>
2022-02-20 22:08:47 -0500

    Mission failed 2

--- omp-stencil-2d.c
+++ omp-stencil-2d.c
@@ -19,21 +19,22 @@
 int main(int argc, char *argv[])
 {
     // Checks to see if arguments are satisfied
-    if (argc != 6)
+    if (argc != 5)
     {
-        printf("usage: ./stencil-2d <num iterations> <input file> <output file> <all-iterations> <thread_count> \n");
+        printf("usage: ./stencil-2d <num iterations> <input file> <output file> <thread_count> \n");
         exit(0);
     }
 
     // Variables are created
-    clock_t begin = clock(); 
     FILE *fp;
     double thread_count= 0;
-    thread_count = atoi(argv[2]);
+    double temp;
+    thread_count = atoi(argv[4]);
     int row = 0, column = 0, iteration = 0;
     char iteration_A[10];
     strcpy(iteration_A, argv[1]);
     iteration = atoi(iteration_A);
+    thread_count = atoi(argv[4]);
 
     // Opens <input file>, reads row and column
     fp = fopen(argv[2], "r");
@@ -41,13 +42,20 @@ int main(int argc, char *argv[])
     fread(&row, sizeof(int), 1, fp);
     fread(&column, sizeof(int), 1, fp);
 
-    // Sets the threadcount
+      // Sets the threadcount
     omp_set_num_threads(thread_count);
+
+    #pragma omp parallel
+    #pragma omp master
+      printf("Running with %d thread(s)\n",omp_get_num_threads());
 
     // BRobey memory allocation
     double **xtmp;
     double **x = malloc2D(row, column);
     double **xnew = malloc2D(row, column);
+
+     // TIMER STARTS
+    clock_t begin = clock();
 
     // Sets the threadcount
     omp_set_num_threads(thread_count);
@@ -56,31 +64,27 @@ int main(int argc, char *argv[])
     fclose(fp);
 
     // Copies Values from x and puts them into xnew, ready to double buffer
-    for(int i = 0; i < row; i++){
+    #pragma omp parallel for
+    for (int i = 0; i < row; i++) {
         for (int j = 0; j < column; j++){
             xnew[i][j] = x[i][j];
         }
     }
-    fp = fopen(argv[4], "w");
-
+  
     // Does Stencil Operation and stores it in a .raw file
-
     for (int i = 0; i < iteration; i++)
-    {
+    {   
+        #pragma omp parallel for reduction(+:x, xnew)
         for (int a = 1; a < row - 1; a++)
         {
-            {
             for (int b = 1; b < column - 1; b++)
-            {
+            {  
                 xnew[a][b] = (x[a - 1][b - 1] + x[a - 1][b] + x[a - 1][b + 1] + x[a][b + 1] + x[a + 1][b + 1] + x[a + 1][b] + x[a + 1][b - 1] + x[a][b - 1] + x[a][b]) / 9.0;
             }
-            }
         }
-        fwrite(&x[0][0], row * column, sizeof(double), fp);
         SWAP_PTR(xnew, x, xtmp);
     }
-    fwrite(&x[0][0], row * column, sizeof(double), fp);
-    fclose(fp);
+    
 
     
     fp = fopen(argv[3], "w");
